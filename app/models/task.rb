24d5cbfd6ab383856bdd332dc01f231e5c05525b
wyang19a <wyang19a@gmail.com>
2020-01-30 16:22:50 -0500

    Add comment, add validation in task model
    1. Added comment inside app/config/initializers/change_time.rb
    2. Added validation in task model (:title is required)

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@
 
 class Task < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
 end
--- config/initializers/change_time.rb
+++ config/initializers/change_time.rb
@@ -3,3 +3,4 @@ class ActiveSupport::TimeWithZone
     strftime('%Y-%m-%d %I:%M %p')
   end
 end
+# strftime will change dates to format 2020-01-30 01:58 PM
